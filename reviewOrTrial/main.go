627d18660fa213dcbc64ca63128a918fa7f7083b
Joshua Biong <joshuabiong7@gmail.com>
2024-02-19 23:45:16 +0800

    move the file to another folder got an error and fix buy runnign command (go mod init main, go mod tidy)

---
+++ reviewOrTrial/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Strings
+	var nameOne string = "Joshua"
+	var nameTwo = "Biong"
+
+	fmt.Println(nameOne, nameTwo)
+	var nameThree string
+	fmt.Println(nameThree)
+	nameFour := "Lywy"
+
+	fmt.Println(nameFour)
+	nameThree = "joshuaBiong"
+	fmt.Println(nameThree)
+
+}
