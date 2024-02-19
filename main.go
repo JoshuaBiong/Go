f957de31852013d8a318d1bed901849ef995c3d2
Joshua Biong <joshuabiong7@gmail.com>
2024-02-19 22:02:14 +0800

    reviewing some static rules I have learn in dart programming language

--- main.go
+++ main.go
@@ -4,7 +4,17 @@ import "fmt"
 
 func main() {
 
+	//Strings
 	var nameOne string = "Joshua"
+	var nameTwo = "Biong"
 
-	fmt.Println(nameOne)
+	fmt.Println(nameOne, nameTwo)
+	var nameThree string
+	fmt.Println(nameThree)
+	nameFour := "Lywy"
+
+	fmt.Println(nameFour)
+	nameThree = "joshuaBiong"
+	fmt.Println(nameThree)
+
 }
